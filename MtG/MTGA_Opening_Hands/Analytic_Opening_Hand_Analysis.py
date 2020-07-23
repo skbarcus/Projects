225a07c195c8747ef6b824d9d439cc6f72d562b0
Unknown <skbarcus@email.wm.edu>
2020-07-23 19:22:46 -0400

    Finished Tables of X Lands on X and Made kdeplots
    Finished making the tables for having X lands on X for 14-20 lands in a 40 card deck. Probs seem correct, but double check in final document. Also made histos for the number of lands in opening hand, but these were illegible with more than three histos per plot. Instead just made one histo for normal vs Bo1 with 17 lands to show how distribution tightens up. Then used seaborn's kdeplot to make plots of the distributions for both normal and Bo1 hands for 14-20 lands in deck. Maybe a few too many, but it gets the point across well about the differences between the two hand types.

--- MtG/MTGA_Opening_Hands/Analytic_Opening_Hand_Analysis.py
+++ MtG/MTGA_Opening_Hands/Analytic_Opening_Hand_Analysis.py
@@ -3,7 +3,9 @@ import numpy as np
 import random
 import collections
 import time
-import scipy.stats as ss #Has hypergeometric calculator.
+import scipy.stats as ss #Has hypergeometric calculator. 
+import seaborn as sns #for nicer graphics
+#sns.set(color_codes=True)
 
 start = time.time() #Start a timer.
 
@@ -174,35 +176,114 @@ plt.show()
 #40 card deck with 17 lands probabilities of having x lands on turn x with y lands in opener. 
 ###
 curve_probs = []
-temp = []
-p = 0
-for i in range(1,4):
-    temp.clear()
-    for j in range(2,7):
-        hpd = ss.hypergeom(deck_size-nopener, 17-i, j-1)
-        for k in range(0,i):
-            p = p + hpd.pmf(j-i+k)
-            print("i, j, k, p = ",i,"",j,"",k,"",p)
-            print("j-i+k = ",j-i+k,"hpd.pmf(j-i+k)",hpd.pmf(j-i+k))
-        if i>=j:
-            temp.append(1.)
-        else:
-            temp.append(p)
-        p = 0
-    temp_arr = np.array(temp)
-    curve_probs.append(temp_arr)
+curve_probs_temp = []
+temp1 = []
+temp2 = []
+p_play = 0
+p_draw = 0
+for z in range(minlands+1,maxlands):
+    curve_probs_temp.clear()
+    for i in range(1,4):
+        temp1.clear()
+        temp2.clear()
+        for j in range(2,7):
+            hpd_play = ss.hypergeom(deck_size-nopener, z-i, j-1)
+            hpd_draw = ss.hypergeom(deck_size-nopener, z-i, j)
+            for k in range(0,i):
+                p_play = p_play + hpd_play.pmf(j-i+k)
+                #p_draw = p_draw + hpd_draw.pmf(j-i+k)
+                #print("i, j, k, p_play = ",i,"",j,"",k,"",p_play)
+                #print("ss.hypergeom(%d,%d,%d) , hpd_play.pmf(%d) = %f" % (deck_size-nopener, z-i, j-1,j-i+k,hpd_play.pmf(j-i+k)))
+            for k in range(0,i+1):
+                p_draw = p_draw + hpd_draw.pmf(j-i+k)
+                #print("i, j, k, p_draw = ",i,"",j,"",k,"",p_draw)
+                #print("ss.hypergeom(%d,%d,%d) , hpd_draw.pmf(%d) = %f" % (deck_size-nopener, z-i, j,j-i+k,hpd_draw.pmf(j-i+k)))
+            if i>=j:
+                temp1.append(1.)
+                temp2.append(1.)
+            else:
+                temp1.append(p_play)
+                temp2.append(p_draw)
+            p_play = 0
+            p_draw = 0
+            #print("********")
+        temp_arr1 = np.array(temp1)
+        temp_arr2 = np.array(temp2)
+        curve_probs_temp.append(temp_arr1)
+        curve_probs_temp.append(temp_arr2)
+    curve_probs_temp_arr = np.array(curve_probs_temp)
+    curve_probs.append(curve_probs_temp_arr)
 
 curve_probs = np.array(curve_probs)
+#Convert to percentages.
+curve_probs = curve_probs*100.
+#Store just two decimals so it's nice to read.
+#curve_probs = [['%.2f' % j for j in i] for i in curve_probs]
+curve_probs = [[['%.2f' % k for k in j] for j in i] for i in curve_probs]
 print(curve_probs)
 
+#Set column labels.
+curve_cols = ("% 2 Lands on 2","% 3 Lands on 3","% 4 Lands on 4","% 5 Lands on 5","% 6 Lands on 6")
+
+#Set row labels.
+curve_rows = []
+for i in range(1,4):
+    curve_rows.append("%d Lands on Play" % i)
+    curve_rows.append("%d Lands on Draw" % i)
+
+#Set column widths.
+curve_widths = []
+for i in range(0,len(curve_cols)):
+    curve_widths.append(0.045)
+
+#Add background colors for the cells.
+curve_colors = []
+for i in range(0,len(curve_rows)):
+    if i%2==0:
+        curve_temp1 = []
+        for j in range(0,len(curve_cols)):
+            curve_temp1.append("cyan")
+        curve_colors.append(curve_temp1)
+    else:
+        curve_temp2 = []
+        for j in range(0,len(curve_cols)):
+            curve_temp2.append("orange")
+        curve_colors.append(curve_temp2)
+print(curve_colors)
+
+curve_col_colors = []
+curve_row_colors = []
+for i in range(0,len(curve_rows)):
+    if i%2==0:
+        curve_row_colors.append("darkcyan")
+    else:
+        curve_row_colors.append("darkorange")
+for i in range(0,len(curve_cols)):
+    curve_col_colors.append("silver")
+
+for i in range(0,5):
+    fig, ax = plt.subplots(figsize=(16,12))
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+    curve_table = plt.table(cellText=curve_probs[i],rowLabels=curve_rows,colLabels=curve_cols,loc='center',colWidths=curve_widths,cellColours=curve_colors,rowColours=curve_row_colors,colColours=curve_col_colors,cellLoc="center")
+    #table.auto_set_font_size(False)
+    #table.set_fontsize(24)
+    curve_table.scale(2.2, 2.2)
+    plt.show()
+
 M = 33  # Total number of cards after drawing opening 7.
-n = 14  # Number of lands left in deck.
-N = 4   # Number of draws to find a land.
-k = 4   # Number of lands we want to draw.
+n = 16  # Number of lands left in deck.
+N = 2   # Number of draws to find a land.
+k = 1   # Number of lands we want to draw.
 import scipy.stats as ss
 hpd = ss.hypergeom(M, n, N)
-p = hpd.pmf(k)+hpd.pmf(4)
-print("Probability of drawing one land = ",p)
+
+p = hpd.pmf(2)+hpd.pmf(3)+hpd.pmf(4)
+print(hpd.pmf(2))
+print(hpd.pmf(3))
+print(hpd.pmf(4))
+print("Probability of drawing one land = ",hpd.pmf(k)+hpd.pmf(k+1))
 """
 M_start = 40  # Total number of cards after drawing opening 7.
 n_start = 17  # Number of lands left in deck.
@@ -212,4 +293,91 @@ hpd_start = ss.hypergeom(M_start, n_start, N_start)
 p_start = hpd_start.pmf(k_start)
 print("Probability of drawing 3 land in opening hand = ",p_start)
 """
+
+#Create histograms of average lands in opening hand for 14-20 lands in a 40 card deck.
+binwidth = 1
+minbin = 0
+maxbin = 7
+nhands = 1e4
+transparency = 0.5
+
+#Create results for histograms.
+histo_results = norm_results
+histo_results = [[float(j) for j in i] for i in histo_results]
+histo_results = np.array(histo_results)
+#Draw a sample of nhands with the proper probability distributions.
+histo_results = histo_results*0.01*nhands
+
+xaxis = []
+for i in range(0,len(histo_results[0])):
+    xaxis.append(i)
+
+labels = []
+for i in range(minlands,maxlands+1):
+    labels.append(i)
+
+kwargs = kwargs = dict(bins=np.arange(minbin, maxbin + binwidth, binwidth),alpha=transparency,density=True,edgecolor="black",histtype='stepfilled')#,label=labels)
+
+#Plot histo for normal and bo1 17 land hands.
+fig, ax = plt.subplots(figsize=(10,10))
+hist1 = ax.hist(xaxis,weights=histo_results[6],**kwargs,label="17 Land Deck Normal Draw")
+hist2 = ax.hist(xaxis,weights=histo_results[7],**kwargs,label="17 Land Deck Bo1 Draw")
+plt.xlabel("Number of Lands in Starting Hand")
+plt.ylabel("Rate of Occurrence")
+plt.title("Distribution of Lands in Opening Hand: Normal Draw vs. MTGA Bo1 Draw")
+ax.legend(prop={'size': 10})
+plt.show()
+
+kde_plots = []
+temp = []
+for i in range(0,histo_results.shape[0]):
+    temp.clear()
+    for j in range(0,histo_results.shape[1]):
+        for k in range(0,int(histo_results[i][j])):
+            temp.append(j)
+    temp_arr = np.array(temp)
+    kde_plots.append(temp_arr)
+#print(kde_plots)        
+
+dist = collections.Counter(kde_plots[6])
+print(dist)
+
+fig, ax = plt.subplots(figsize=(10,10))
+#hist1 = ax.hist(kde_plots[6],**kwargs)
+for i in range(0,histo_results.shape[0]):
+    if i%2==0:
+        sns.kdeplot(kde_plots[i],ax=ax,linewidth=2.0,gridsize=200,bw=.42,legend=True,label="%d Land Deck" % (minlands+i/2))#,shade=True)#,bw=.42)
+#ax.legend(labels,prop={'size': 10})#Legend is the shaded colors if kdeplot is shaded.
+plt.xlabel("Number of Lands in Starting Hand")
+plt.ylabel("Rate of Occurrence")
+plt.title("Distribution of Lands in a Normal Opening Hand Based on Number of Lands in Deck")
+plt.show()
+
+fig, ax = plt.subplots(figsize=(10,10))
+#hist1 = ax.hist(kde_plots[6],**kwargs)
+for i in range(0,histo_results.shape[0]):
+    if i%2==1:
+        sns.kdeplot(kde_plots[i],ax=ax,linewidth=2.0,gridsize=200,bw=.62,legend=True,label="%d Land Deck" % (minlands+i/2))#,shade=True)#,bw=.42)
+#ax.legend(labels,prop={'size': 10})#Legend is the shaded colors if kdeplot is shaded.
+#ax.plot(linewidth=2.0)
+plt.xlabel("Number of Lands in Starting Hand")
+plt.ylabel("Rate of Occurrence")
+plt.title("Distribution of Lands in an MTGA Bo1 Opening Hand Based on Number of Lands in Deck")
+plt.show()
+
+"""
+histos = []
+#histos = [None] * 10
+#plt.style.use('seaborn-white')
+#Plot histo for all normal hands 14-20.
+fig, ax = plt.subplots(figsize=(10,10))
+for i in range(0+2,histo_results.shape[0]-2):
+    if i%2==0:
+        #histos.append([])
+        histos.append(ax.hist(xaxis,weights=histo_results[i],**kwargs))
+        #sns.kdeplot(xaxis)
+ax.legend(labels,prop={'size': 10})
+plt.show()
+"""
+
 print("The script took %.2f seconds (%.2f minutes or %.2f hours) to run." % (time.time() - start, (time.time() - start)/60.,(time.time() - start)/60./60.)) #Print time to run.
